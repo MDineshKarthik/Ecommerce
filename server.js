a07a68d939729709f81af79d8cc4be4fb9e023e9
ShivaChary1 <shivachary.ul@gmail.com>
2024-07-13 19:42:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+const engine = require("ejs-mate");
+const { sign } = require("crypto");
+
+app.engine('ejs', engine);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+
+app.get("/login",(req,res)=>{
+    res.render('user/login')
+})
+
+app.get("/signup",(req,res)=>{
+    res.render("user/signup")
+})
+
+
+app.listen(8080,()=>{
+    console.log("app is runnig on 8080")
+})
